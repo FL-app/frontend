237ff420f2440c85ded8fa825411dbcf36216412
W66LIW <meerim.art.b@gmail.com>
2023-11-02 16:15:59 +0300

    refactor(carousel): migration Carousel to TS & installing @types/react-slick
    migration Carousel to TS & installing @types/react-slick (deletion react-slick)

--- src/pages/Welcome/Welcome.js
+++ src/pages/Welcome/Welcome.js
@@ -1,13 +1,10 @@
-import React from 'react';
 import './Welcome.scss';
 import { Link } from 'react-router-dom';
 import Button from '../../components/Button/Button.tsx';
 import Carousel from '../../components/Carousel/Carousel';
 import { ROUTES } from '../../constants';
-import { testTS } from '../../testTS.ts';
 
 export function Welcome() {
-	testTS();
 	return (
 		<main className="welcome">
 			<div className="welcome_container">
