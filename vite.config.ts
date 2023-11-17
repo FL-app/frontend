e321ff5831ac7fd68dac41ec6a823af9e755fb7b
KirillDemyanenko <demyanenkokg@gmail.com>
2023-11-17 10:59:14 +0300

    config(vite.config.ts): optimizeDeps

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
 	base: '/',
 	plugins: [react()],
+	optimizeDeps: {
+		force: true,
+		esbuildOptions: {
+			loader: {
+				'.js': 'jsx',
+			},
+		},
+	},
 });
