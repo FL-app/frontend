20f2fc5bb9f8f3d19e476ce6c1c65dcf9eb3a355
KirillDemyanenko <demyanenkokg@gmail.com>
2023-11-20 19:41:05 +0300

    refactor(ButtonUserLocation.tsx): convert to .tsx

---
+++ src/components/ButtonUserLocation/ButtonUserLocation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './ButtonUserLocation.scss';
+
+interface ButtonUserLocationProps {
+	handleClick: () => void;
+}
+
+export default function ButtonUserLocation(props: ButtonUserLocationProps) {
+	const { handleClick } = props;
+
+	return (
+		<button
+			className="buttonUserLocation"
+			type="button"
+			aria-label="Мое местоположение на карте"
+			onClick={handleClick}
+		/>
+	);
+}
--- src/pages/TrackingMap/TrackingMap.jsx
+++ src/pages/TrackingMap/TrackingMap.jsx
@@ -8,7 +8,7 @@ import MainLayout from '../../layouts/MainLayout.jsx';
 import './TrackingMap.scss';
 import geotag from '../../images/geotag_map.svg';
 import { friendsLocation } from './friendsLocation'; // TODO делать запрос к серверу для получения списка друзей, брать их координаты
-import ButtonUserLocation from '../../components/ButtonUserLocation/ButtonUserLocation.jsx';
+import ButtonUserLocation from '../../components/ButtonUserLocation/ButtonUserLocation.tsx';
 import RoutesPath from '../../constants/enums/routesPath';
 import { sendCoords } from '../../store/thunk/sendCoords';
 
