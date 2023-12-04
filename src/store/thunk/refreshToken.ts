257a371737785a3cc59a57ec4fd703ae1cb30fb0
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-04 19:04:42 +0300

    refactor(refreshToken.ts): convert to .ts and to async function

--- src/store/thunk/refreshToken.ts
+++ src/store/thunk/refreshToken.ts
@@ -1,20 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { refreshToken as refresh } from '../../untils/mianApi';
 
-export const refreshToken = createAsyncThunk(
+const refreshToken = createAsyncThunk(
 	'jwt/refresh',
-	async (payload, thunkAPI) => {
+	async (payload: string, thunkAPI) => {
 		try {
 			const response = await refresh(payload);
-			const data = await response.json();
+			const data = (await response.json()) as { access: string };
 			if (!response.ok) {
 				throw new Error(JSON.stringify(data));
 			}
 			localStorage.setItem('access_token', data.access);
-
 			return data;
 		} catch (error) {
-			return thunkAPI.rejectWithValue(error.message);
+			if (error instanceof Error) {
+				return thunkAPI.rejectWithValue(error.message);
+			}
+			return error;
 		}
 	}
 );
+
+export default refreshToken;
