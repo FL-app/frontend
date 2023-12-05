bd8658494a418c0a64817c85f1632a7cf871d172
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-05 18:36:56 +0300

    refactor(loginUser.ts): convert to .ts

---
+++ src/store/thunk/loginUser.ts
@@ -0,0 +1,28 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { login } from '../../untils/mianApi';
+
+const loginUser = createAsyncThunk(
+	'jwt/create',
+	async (payload: { email: string; password: string }, thunkAPI) => {
+		try {
+			const response = await login(payload);
+			if (!response.ok) {
+				throw new Error(JSON.stringify(response.json()));
+			}
+			const data = (await response.json()) as {
+				access: string;
+				refresh: string;
+			};
+			localStorage.setItem('access_token', data.access);
+			localStorage.setItem('refresh_token', data.refresh);
+			return data;
+		} catch (error) {
+			if (error instanceof Error) {
+				return thunkAPI.rejectWithValue(error.message);
+			}
+			return error;
+		}
+	}
+);
+
+export default loginUser;
