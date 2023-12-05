be912b84c097d00878e90f34014a91d1a0ebb5ef
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-05 17:57:57 +0300

    refactor(pages\index.ts): convert to .ts

---
+++ src/pages/index.ts
@@ -0,0 +1,11 @@
+export * from './Registration/Registration';
+export * from './Login/Login';
+export * from './TermsOfUse/TermsOfUse';
+export { default as PrivacyPolicy } from './PrivacyPolicy/PrivacyPolicy';
+export { default as Friends } from './Friends/Friends';
+export { default as Welcome } from './Welcome/Welcome';
+export { default as AccessGeo } from './AccessGeo/AccessGeo';
+export { default as AccessGeoError } from './AccessGeoError/AccessGeoError';
+export { default as Profile } from './Profile/Profile';
+export { default as AccessAge } from './AccessAge/AccessAge';
+export { default as ComingSoon } from './ComingSoon/ComingSoon';
