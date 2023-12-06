54988e507500bb593b8c71cecdf57b5272fde993
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-06 14:34:53 +0300

    fix(loginUser.ts): types

--- src/store/thunk/loginUser.ts
+++ src/store/thunk/loginUser.ts
@@ -1,26 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { login } from '../../untils/mainApi';
+import { LoginDTO, TokensDTO } from '../../constants/apiTemplate';
 
 const loginUser = createAsyncThunk(
 	'jwt/create',
-	async (payload: { email: string; password: string }, thunkAPI) => {
+	async (payload: LoginDTO, thunkAPI) => {
 		try {
 			const response = await login(payload);
 			if (!response.ok) {
-				throw new Error(JSON.stringify(response.json()));
+				return thunkAPI.rejectWithValue(JSON.stringify(await response.json()));
 			}
-			const data = (await response.json()) as {
-				access: string;
-				refresh: string;
-			};
+			const data = (await response.json()) as TokensDTO;
 			localStorage.setItem('access_token', data.access);
 			localStorage.setItem('refresh_token', data.refresh);
 			return data;
 		} catch (error) {
 			if (error instanceof Error) {
-				return thunkAPI.rejectWithValue(error.message);
+				JSON.stringify({ detail: error.message });
 			}
-			return error;
+			return JSON.stringify({ detail: 'unknown error' });
 		}
 	}
 );
