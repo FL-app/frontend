b3237ec21fda2525745038f19a7ec62606399073
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-07 17:39:50 +0300

    refactor(AppContext.tsx): remove unused export

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -2,7 +2,7 @@ import { createContext, useState, useContext, ReactElement } from 'react';
 import { IUserData } from '../constants/tempUserData';
 import mockCurrentUser from './mock';
 
-export const AppContext = createContext<IUserData>(mockCurrentUser);
+const AppContext = createContext<IUserData>(mockCurrentUser);
 
 interface AppContextProviderProps {
 	children: ReactElement | ReactElement[];
