9780baef8dc2a098ae90ca7038b087a6c0759d25
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-09 12:57:33 +0300

    feat(getCurrentUser.ts): add UserDTO interface

--- src/store/thunk/getCurrentUser.ts
+++ src/store/thunk/getCurrentUser.ts
@@ -1,18 +1,32 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { getCurrentUser as getUser } from '../../untils/mainApi';
 
+export interface UserDTO {
+	id: number;
+	email: string;
+	username: string;
+	first_name: string;
+	last_name: string;
+	longitude: number;
+	latitude: number;
+	status: string;
+	userpic: string | null;
+}
+
 const getCurrentUser = createAsyncThunk(
 	'user/me',
 	async (payload: string, thunkAPI) => {
 		try {
 			const response = await getUser(payload);
-			if (!response.ok) throw new Error(JSON.stringify(response.json()));
-			return await response.json();
-		} catch (err) {
-			if (err instanceof Error) {
-				return thunkAPI.rejectWithValue(err.message);
+			if (!response.ok) {
+				return thunkAPI.rejectWithValue(JSON.stringify(await response.json()));
 			}
-			return err;
+			return JSON.stringify(await response.json());
+		} catch (error) {
+			if (error instanceof Error) {
+				return thunkAPI.rejectWithValue(JSON.stringify(error));
+			}
+			return JSON.stringify(error);
 		}
 	}
 );
