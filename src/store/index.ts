23b1e361cd63da0635ca4d34a277f624edaebc4d
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-12 15:59:41 +0300

    feat(store): add rtk query middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,20 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import locationSlice from './slices/location';
 import userSlice from './slices/user';
+import { tokensApi } from './rtk/tokensApi';
+import tokensSlice from './slices/tokens';
 
 const rootReducer = combineReducers({
 	location: locationSlice,
 	user: userSlice,
+	tokens: tokensSlice,
+	[tokensApi.reducerPath]: tokensApi.reducer,
 });
 
 const store = configureStore({
 	reducer: rootReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware().concat(tokensApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
