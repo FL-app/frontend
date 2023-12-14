db0b0be87cae45b336d4fd82f9a0ec99e0e8c6c0
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-14 20:08:50 +0300

    feat(app): add middleware

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,21 +3,22 @@ import locationSlice from './slices/location';
 import userSlice from './slices/user';
 import { tokensApi } from './rtk/tokensApi';
 import tokensSlice from './slices/tokens';
+import { userApi } from './rtk/userApi';
 
 const rootReducer = combineReducers({
 	location: locationSlice,
 	user: userSlice,
 	tokens: tokensSlice,
 	[tokensApi.reducerPath]: tokensApi.reducer,
+	[userApi.reducerPath]: userApi.reducer,
 });
 
 const store = configureStore({
 	reducer: rootReducer,
 	middleware: (getDefaultMiddleware) =>
-		getDefaultMiddleware().concat(tokensApi.middleware),
+		getDefaultMiddleware()
+			.concat(tokensApi.middleware)
+			.concat(userApi.middleware),
 });
-
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
 
 export default store;
