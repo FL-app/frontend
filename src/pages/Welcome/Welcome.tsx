9a4bece4d64fabaf47a41ff9156aa6ccfe86bd46
KirillDemyanenko <demyanenkokg@gmail.com>
2023-12-20 19:37:03 +0300

    chore(Welcome.tsx): remove unused

--- src/pages/Welcome/Welcome.tsx
+++ src/pages/Welcome/Welcome.tsx
@@ -6,31 +6,29 @@ import RoutesPath from '../../constants/enums/routesPath';
 
 function Welcome() {
 	return (
-		<main className="welcome">
-			<div className="welcome_container">
-				<Carousel />
-				<div className="welcome__button-container">
-					<Link to={RoutesPath.accessAge}>
-						<Button
-							label="Зарегистрироваться"
-							type="button"
-							size="large"
-							color="primary"
-							disabled={false}
-						/>
-					</Link>
-					<Link to={RoutesPath.login}>
-						<Button
-							label="Войти"
-							type="button"
-							size="large"
-							color="secondary"
-							disabled={false}
-						/>
-					</Link>
-				</div>
+		<>
+			<Carousel />
+			<div className="welcome__button-container">
+				<Link to={RoutesPath.accessAge}>
+					<Button
+						label="Зарегистрироваться"
+						type="button"
+						size="large"
+						color="primary"
+						disabled={false}
+					/>
+				</Link>
+				<Link to={RoutesPath.login}>
+					<Button
+						label="Войти"
+						type="button"
+						size="large"
+						color="secondary"
+						disabled={false}
+					/>
+				</Link>
 			</div>
-		</main>
+		</>
 	);
 }
 
